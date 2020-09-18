c72b76d689ce9604a6a7acb997a82947ed68376b
Daniel Bashary <dbashary@student.touro.edu>
2020-09-18 14:37:51 +0300

    Question 2 is in a comment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+//Daniel Bashary
+//for question 2: The only thing that I can tell is that it seems as if Shakespeare uses bigger words a lot more often.
+
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hi");
+
+    public static void main(String[] args) throws FileNotFoundException {
+        findWordLengthFrequency Word_Length = new findWordLengthFrequency();
+        File A_Tale_of_Two_Cities = new File("C:\\Users\\Daniel Bashary\\IdeaProjects\\author-fingerprint-basharyd\\src\\A Tale of Two Cities.txt");
+        File Great_Expectations = new File("C:\\Users\\Daniel Bashary\\IdeaProjects\\author-fingerprint-basharyd\\src\\Great Expectations.txt");
+        File Hamlet = new File("C:\\Users\\Daniel Bashary\\IdeaProjects\\author-fingerprint-basharyd\\src\\Hamlet.txt");
+        File Macbeth = new File("C:\\Users\\Daniel Bashary\\IdeaProjects\\author-fingerprint-basharyd\\src\\Macbeth.txt");
+        Word_Length.printWordCount(Word_Length.findWordLengthFrequencyMethod(A_Tale_of_Two_Cities), "A Tale of Two Cities");
+        Word_Length.printWordCount(Word_Length.findWordLengthFrequencyMethod(Great_Expectations), "Great Expectations");
+        Word_Length.printWordCount(Word_Length.findWordLengthFrequencyMethod(Hamlet), "Hamlet");
+        Word_Length.printWordCount(Word_Length.findWordLengthFrequencyMethod(Macbeth), "Macbeth");
     }
 }
---
+++ src/main/java/findWordLengthFrequency.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static jdk.nashorn.internal.objects.NativeMath.round;
+
+
+public class findWordLengthFrequency implements IWordFrequency {
+    public Map<Integer, Double> findWordLengthFrequencyMethod(File file) throws FileNotFoundException {
+        Scanner s = new Scanner(file);
+        ArrayList<String> words = new ArrayList<>();
+        ArrayList<Integer> word_count = new ArrayList<>();
+        Map<Integer, Double> wordLength = new HashMap<>();
+        while (s.hasNext()){
+            words.add(s.next());
+        }
+        s.close();
+        for(String i : words)
+        {
+            String resultString1 = i.replaceAll("\\p{Punct}", "");
+            word_count.add(resultString1.length());
+        }
+        Collections.sort(word_count);
+        for (int i = 0; i < Collections.max(word_count); i++) {
+            if (!wordLength.containsKey(i)) {
+                double a = Collections.frequency(word_count, i);
+                double percentage = a / word_count.size();
+                wordLength.put(i, percentage);
+            }
+        }
+        return wordLength;
+    }
+    public void printWordCount(Map<Integer, Double> map, String title) throws FileNotFoundException {
+        PrintWriter pw = new PrintWriter(new File(title + ".txt"));
+        pw.println("FileName: " + title + " Histogram");
+        pw.println("Contents:");
+        pw.println("WordLength Freq");
+        for (int word : map.keySet())
+        {
+            Double word_count = map.get(word);
+            pw.println(word + " " + word_count);
+        }
+        pw.close();
+    }
+}
